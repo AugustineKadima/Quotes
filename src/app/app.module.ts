558fa9fd527eabf031991563af5dea7c3afe3dd3
C0d3president <kadimasam@gmail.com>
2021-07-16 14:44:08 +0300

    Create quote-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesListComponent } from './quotes-list/quotes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesListComponent
   ],
   imports: [
     BrowserModule,
