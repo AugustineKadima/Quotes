5a3ed0cb6ff1d9848dafeee215e7dab54c0d98d5
C0d3president <kadimasam@gmail.com>
2021-07-16 21:16:37 +0300

    populate quotes

--- src/app/quotes-list/quotes-list.component.ts
+++ src/app/quotes-list/quotes-list.component.ts
@@ -9,8 +9,10 @@ import { QuoteModel } from '../quote-model';
 export class QuotesListComponent implements OnInit {
 
  quotes:QuoteModel[] = [
-    new QuoteModel(1, "Never give up", "Mohamed Ali", "Kadima"),
-    new QuoteModel(2, "Arise and shine", "Joseph", "Cleophas")
+    new QuoteModel(1, "Everyone thinks of changing the world, but no one thinks of changing himself", " Leo Tolstoy", "Augustine"),
+    new QuoteModel(2, "Never doubt that a small group of thoughtful, committed, citizens can change the world", "Margaret Mead", "Anira"),
+    new QuoteModel(3, "Education is the most powerful weapon which you can use to change the world", "Nelson Mandela", "Brian"),
+    new QuoteModel(3, "Why do you go away? So that you can come back. So that you can see the place you came from with new eyes and extra colors. And the people there see you differently, too. Coming back to where you started is not the same as never leaving", "Terry Pratchett, A Hat Full of Sky", "Brian")
   ]
 
   constructor() { }
